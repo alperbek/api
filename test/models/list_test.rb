003c9496c0663f8648db3dd6aa86b4e8ded8b469
Mete Kabak <kabakmete@gmail.com>
2016-03-07 00:56:13 +0200

    #test list assert_equal test

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  
-	test "invalid without a title" do
-		l = lists(:grocery)
 
-		assert l.valid? , "List was not valid"
+	setup do
+		@list = lists(:grocery)
+		@grocery = groceries(:one)
+	end
+
+	test "invalid without a title" do
+		assert @list.valid? , "List was not valid"
 	end
 
 	test "invalid title gives error message" do
-		l = lists(:person)
-		l.title = nil
-		l.valid?
+		@list.title = nil
+		@list.valid?
 
-		assert_match /can't be blank/, l.errors[:title].join,
+		assert_match /can't be blank/, @list.errors[:title].join,
 		"Presence error not found on list"
+	end	
+
+	test "should respond to grocery" do
+		assert_respond_to @list, :grocery
 	end
 
+	test "should contain only items that belongs to list" do
+		assert_equal @list , @grocery.list
+	end
 
 end

7e9773513e29dbee99be920a1e4d7a99e37bfb54
Mete Kabak <kabakmete@gmail.com>
2016-03-18 04:19:42 +0200

    fixing route tests

--- spec/routing/lists_routing_spec.rb
+++ spec/routing/lists_routing_spec.rb
@@ -1,26 +1,26 @@
 require "rails_helper"
 
-RSpec.describe ListsController, type: :routing do
+RSpec.describe V1::ListsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/lists").to route_to("lists#index")
+      expect(:get => "/v1/lists").to route_to("v1/lists#index")
     end
 
     it "routes to #create" do
-      expect(:post => "/lists").to route_to("lists#create")
+      expect(:post => "/v1/lists").to route_to("v1/lists#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/lists/1").to route_to("lists#update", :id => "1")
+      expect(:put => "/v1/lists/1").to route_to("v1/lists#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/lists/1").to route_to("lists#update", :id => "1")
+      expect(:patch => "/v1/lists/1").to route_to("v1/lists#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/lists/1").to route_to("lists#destroy", :id => "1")
+      expect(:delete => "/v1/lists/1").to route_to("v1/lists#destroy", :id => "1")
     end
 
   end
